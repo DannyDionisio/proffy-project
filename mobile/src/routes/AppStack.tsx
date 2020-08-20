c804d815d37a77c6b786bec07cec3902972f4fb5
Daniela Dionisio <dannyy.dionisio@gmail.com>
2020-08-20 17:27:29 +0100

    Routes, Giveclasses and teacherlist pages working

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Landing from "../pages/Landing";
+import GiveClasses from "../pages/GiveClasses";
+import StudyTabs from "./StudyTabs";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -10,7 +12,9 @@ function AppStack() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="Landing" component={Landing}></Screen>
+        <Screen name="Landing" component={Landing} />
+        <Screen name="GiveClasses" component={GiveClasses} />
+        <Screen name="Study" component={StudyTabs} />
       </Navigator>
     </NavigationContainer>
   );
